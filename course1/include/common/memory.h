bd1c1cf93d1e5a7e89537f32f63fe8869bd019f1
kloyd <kelly.loyd@icloud.com>
2018-03-05 14:24:10 -0600

    More documentation for memory functions.

--- course1/include/common/memory.h
+++ course1/include/common/memory.h
@@ -96,7 +96,7 @@ void set_all(char * ptr, char value, unsigned int size);
 void clear_all(char * ptr, unsigned int size);
 
 /**
- * @brief Memory Move characters in an array.
+ * @brief Memory Move bytes in an array.
  *
  * Take two byte pointers and a length of bytes, move from
  * source location to destination location for length.
@@ -108,16 +108,81 @@ void clear_all(char * ptr, unsigned int size);
  */
 uint8_t * my_memmove(uint8_t *src, uint8_t *dst, size_t length);
 
+/**
+ * @brief Memory Copy bytes in an array.
+ *
+ * Take two byte pointers and a length of bytes, copy from
+ * source location to destination location for length.
+ * @param src Pointer to source array.
+ * @param dst Pointer to destination array.
+ * @param length Number of elements to move.
+ *
+ * @return Pointer to source array.
+ */
 uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length);
 
+/**
+ * @brief Memory Set bytes in an array.
+ *
+ * Take a byte pointer and a length of bytes,
+ * set each location to the passed in value.
+ * @param src Pointer to source array.
+ * @param length Number of elements to move.
+ * @param value - Value to store.
+ *
+ * @return Pointer to source array.
+ */
 uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value);
 
+/**
+ * @brief Memory zero bytes in an array.
+ *
+ * Take a byte pointer and a length of bytes,
+ * clear number of bytes specified.
+ * @param src Pointer to source array.
+ * @param length Number of elements to move.
+ *
+ * @return Pointer to source array.
+ */
 uint8_t * my_memzero(uint8_t * src, size_t length);
 
+/**
+ * @brief Memory Move bytes in an array.
+ *
+ * Take two byte pointers and a length of bytes, move from
+ * source location to destination location for length.
+ * @param src Pointer to source array.
+ * @param dst Pointer to destination array.
+ * @param length Number of elements to move.
+ *
+ * @return Pointer to source array.
+ */
 uint8_t * my_reverse(uint8_t * src, size_t length);
 
+/**
+ * @brief Memory Move bytes in an array.
+ *
+ * Take two byte pointers and a length of bytes, move from
+ * source location to destination location for length.
+ * @param src Pointer to source array.
+ * @param dst Pointer to destination array.
+ * @param length Number of elements to move.
+ *
+ * @return Pointer to source array.
+ */
 int32_t * reserve_words(size_t length);
 
+/**
+ * @brief Memory Move bytes in an array.
+ *
+ * Take two byte pointers and a length of bytes, move from
+ * source location to destination location for length.
+ * @param src Pointer to source array.
+ * @param dst Pointer to destination array.
+ * @param length Number of elements to move.
+ *
+ * @return Pointer to source array.
+ */
 void free_words(uint32_t * src);
 
 #endif /* __MEMORY_H__ */
